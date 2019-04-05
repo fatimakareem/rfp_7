408654534395d42a6927f45305e96c1a30012aaa
unknown <fatima.karim@brainplow.com>
2019-04-05 12:44:50 +0500

    hhh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,8 +18,8 @@ export class AppComponent implements OnInit {
     constructor( private router: Router) { }
 
     ngOnInit() {
-        // $.material.options.autofill = true;
-        // $.material.init();
+        $.material.options.autofill = true;
+        $.material.init();
         this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
             if (window.outerWidth > 991) {
                 window.document.children[0].scrollTop = 0;
